9b2a82ee1bd5a05a88b9ec6af89b0980f806e406
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2026-06-06 08:10:41 -0700

    Initial Commit

---
+++ Recap/RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapApp.swift
+//  Recap
+//
+//  Created by David Roy on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
